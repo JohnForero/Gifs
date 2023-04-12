67aefaa984ec47c23f01496d448777f719faf8f7
John Forero <sgm30012004@gmail.com>
2023-04-11 19:11:53 -0500

    login final

---
+++ script_login.js
@@ -0,0 +1,22 @@
+let nombreUsuario = document.querySelector('.input__user');
+let contraseña = document.querySelector('.input__password');
+let user = 'john';
+let password = '1234';
+let login = document.querySelector('.container');
+let modal = document.querySelector('.container__modal');
+
+
+let imprimir = () => {
+    
+ if (nombreUsuario.value == user && contraseña.value == password) {
+     window.location.href='buscador.html';
+ } else {
+   login.style.display = 'none';
+   modal.style.display = 'flex';
+ }
+}
+
+let cerrarModal = () => {
+    login.style.display = 'flex';
+    modal.style.display = 'none';
+}
